1ae7b676c4075c6df42ba0cdd0434eb9fd52df81
rajaprhub <98698646+rajaprhub@users.noreply.github.com>
2023-03-21 22:32:41 +0530

    inputsr

--- listnameapp/src/App.js
+++ listnameapp/src/App.js
@@ -1,11 +1,11 @@
 import logo from './logo.svg';
 import './App.css';
-
+import { Addform } from './Components/Addform';
 function App() {
   return (
     <div className="App">
               <h1> Empolyee app</h1>
-              
+              <Addform/>
     </div>
   );
 }
--- listnameapp/src/Components/addform.jsx
+++ listnameapp/src/Components/addform.jsx
@@ -1,13 +1,25 @@
-import React from 'react'
+import React, { useEffect } from 'react'
+import { useState } from 'react'
+
+import styles from '../Styles/addform.module.css'
 
 
-
-
-
-export const addform = () => {
+export const Addform = () => {
   return (
     <>
+         
+         <div className={styles.container}>
+          <form  >
+             <input type="text"      name="name"           placeholder=" enter name"/>
+             <input type="email"      name="email"         placeholder="email"/>
+             <input type="text"       name="company_name"  placeholder=" enter company_name"/>
+             <input type="number"     name="phone_number"   placeholder="enter phone_number"/>
+             <input type="text"       name="requirement "   placeholder=" enter requirement"/>
+             <input type="text"       name="lead_types_id"   placeholder=" enter lead_types_id "/>
+             <button type="submit" className={styles.btn}>  Add Now</button>
+          </form>
 
+        </div>
 
     </>
   )
