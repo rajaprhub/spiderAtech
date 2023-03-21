4ac1ac07409d33593e137396c222bb5ec53d369e
rajaprhub <98698646+rajaprhub@users.noreply.github.com>
2023-03-21 22:44:12 +0530

    api err handle

--- listnameapp/src/Components/addform.jsx
+++ listnameapp/src/Components/addform.jsx
@@ -1,17 +1,49 @@
 import React, { useEffect } from 'react'
 import { useState } from 'react'
-
 import styles from '../Styles/addform.module.css'
 
+import { useForm } from 'react-hook-form';
 
 export const Addform = () => {
+
+    const [data,setData] = useState ( {  
+        name: "", 
+        email:"",
+        company_name:"",
+        phone_number:"",
+        requirement:"",
+    } )
+
+
+    const getdummypre = async  ()=>{
+        try{
+          let res = await fetch(`https://dashboard.omnisellcrm.com/api/store`)
+          res = await res.json()
+           console.log(res)
+         }
+            catch (error) {
+            console.log(error)
+        }
+       }
+
+
+     
+    useEffect( ()=>{
+        getdummypre()
+     },[])
+
+
+
+
+
+
   return (
     <>
          
          <div className={styles.container}>
           <form  >
-             <input type="text"      name="name"           placeholder=" enter name"/>
-             <input type="email"      name="email"         placeholder="email"/>
+             <input type="text"       name="name"          placeholder=" enter name"/>
+             <input type="email"      name="email"         placeholder="enter email"/>
              <input type="text"       name="company_name"  placeholder=" enter company_name"/>
              <input type="number"     name="phone_number"   placeholder="enter phone_number"/>
              <input type="text"       name="requirement "   placeholder=" enter requirement"/>
